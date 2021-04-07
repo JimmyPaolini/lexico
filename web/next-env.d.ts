86a3518b9542e99c29dc78e7e858289917a90834
Jimmy Paolini <jimmypaolini@gmail.com>
2021-04-07 14:19:20 -0400

    esModuleInterop false, module commonjs

--- web/next-env.d.ts
+++ web/next-env.d.ts
@@ -1,2 +1,12 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare module "*.graphql" {
+  import { DocumentNode } from "graphql"
+  const value: DocumentNode
+  export default value
+}
+
+declare module "react-lazy-load"
+
+declare module "react-lines-ellipsis"
