804e72750af1c497d5357fe8ee6096e9c3da4d29
Jimmy Paolini <jimmypaolini@gmail.com>
2021-04-08 16:17:16 -0400

    testing path module import

--- web/src/components/accessories/SearchBar.tsx
+++ web/src/components/accessories/SearchBar.tsx
@@ -1,3 +1,4 @@
+import SwitchEnLa from "@components/search/SwitchEnLa"
 import { Grid } from "@material-ui/core"
 import CircularProgress from "@material-ui/core/CircularProgress"
 import IconButton from "@material-ui/core/IconButton"
@@ -7,7 +8,6 @@ import { makeStyles } from "@material-ui/core/styles"
 import MenuIcon from "@material-ui/icons/Menu"
 import SearchIcon from "@material-ui/icons/Search"
 import React, { Dispatch, SetStateAction, useContext, useRef } from "react"
-import SwitchEnLa from "src/components/search/SwitchEnLa"
 import { pascalCase } from "../../utils/string"
 import { Context } from "../Context"
 
