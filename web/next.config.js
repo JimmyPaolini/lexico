6edd5484e5efd7822ff10e659eab22a9e80db146
Jimmy Paolini <jimmypaolini@gmail.com>
2021-04-08 20:13:57 -0400

    just ignoreBuildErrors

--- web/next.config.js
+++ web/next.config.js
@@ -1,3 +1,4 @@
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin")
 module.exports = {
   // future: { webpack5: true },
   webpack: (config) => {
@@ -25,17 +26,6 @@ module.exports = {
     // config.resolve.modules.push(path.resolve("./"))
 
     // config.context = require("path").resolve(__dirname + "/../")
-
-    // remove existing plugin
-    config.plugins = config.plugins.filter((plugin) => {
-      return plugin.constructor.name !== "ForkTsCheckerWebpackPlugin"
-    })
-    // only report errors on a matcher that doesn't match anything
-    config.plugins.push(
-      new require("fork-ts-checker-webpack-plugin")({
-        reportFiles: ["does-not-exist"],
-      }),
-    )
 
     return config
   },
