79ca6c4fced239824d61f2a26f659f857627ecf6
Jimmy Paolini <jimmypaolini@gmail.com>
2021-04-08 20:03:41 -0400

    Trying webpack plugin

--- web/next.config.js
+++ web/next.config.js
@@ -26,10 +26,22 @@ module.exports = {
 
     // config.context = require("path").resolve(__dirname + "/../")
 
+    // remove existing plugin
+    config.plugins = config.plugins.filter((plugin) => {
+      return plugin.constructor.name !== "ForkTsCheckerWebpackPlugin"
+    })
+    // only report errors on a matcher that doesn't match anything
+    config.plugins.push(
+      new require("fork-ts-checker-webpack-plugin")({
+        reportFiles: ["does-not-exist"],
+      }),
+    )
+
     return config
   },
-  // typescript: { // Dangerous; manually typecheck before run/build
-  //   ignoreBuildErrors: true,
-  //   transpileOnly: true,
-  // },
+  typescript: {
+    // Dangerous; manually typecheck before run/build
+    ignoreBuildErrors: true,
+    //   transpileOnly: true,
+  },
 }
