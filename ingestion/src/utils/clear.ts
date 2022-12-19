a8c5da5022e62ee4125614ff29d092dfb5d20fca
Jimmy Paolini <jimmypaolini@gmail.com>
2022-12-19 17:40:00 -0500

    feat: new backup script

--- ingestion/src/utils/clear.ts
+++ ingestion/src/utils/clear.ts
@@ -3,10 +3,10 @@ import { EntityTarget, getConnection } from 'typeorm'
 import Entry from '../../../server/src/entity/dictionary/Entry'
 import Translation from '../../../server/src/entity/dictionary/Translation'
 import Word from '../../../server/src/entity/dictionary/Word'
-import Author from '../../../server/src/entity/literature/Author'
-import Book from '../../../server/src/entity/literature/Book'
-import Line from '../../../server/src/entity/literature/Line'
-import Text from '../../../server/src/entity/literature/Text'
+import Author from '../../../server/src/entity/library/Author'
+import Book from '../../../server/src/entity/library/Book'
+import Line from '../../../server/src/entity/library/Line'
+import Text from '../../../server/src/entity/library/Text'
 import User from '../../../server/src/entity/user/User'
 import log from '../../../utils/log'
 
@@ -25,12 +25,12 @@ export async function clearDictionary(): Promise<void> {
 }
 
 export async function clearLiterature(): Promise<void> {
-  log.info(`clearing literature`)
+  log.info(`clearing library`)
   await clearEntity(Line)
   await clearEntity(Text)
   await clearEntity(Book)
   await clearEntity(Author)
-  log.info(`cleared literature`)
+  log.info(`cleared library`)
 }
 
 export async function clearUsers(): Promise<void> {
