6cb4eae01bcb9b66c0d907cefe8f72d9a4bec935
Jimmy Paolini <jimmypaolini@gmail.com>
2023-01-08 22:06:30 -0500

    fix: 🐛 hydration error fixed

--- web/src/components/accessories/Link.tsx
+++ web/src/components/accessories/Link.tsx
@@ -1,31 +1,14 @@
 // https://gist.github.com/kachar/028b6994eb6b160e2475c1bb03e33e6a#file-linkmuiv5-tsx
-import { forwardRef } from 'react'
+import NextLink from 'next/link'
 
-import NextLink, { LinkProps as NextLinkProps } from 'next/link'
+import { LinkProps, Link as MuiLink } from '@mui/material'
 
-import { Link as LinkMUI, LinkProps as LinkMUIProps } from '@mui/material'
-
-export type LinkProps = Omit<LinkMUIProps, 'href' | 'classes'> &
-  Pick<NextLinkProps, 'href' | 'as' | 'prefetch'>
-
-const Link = forwardRef<HTMLAnchorElement, LinkProps>(
-  ({ href, as, prefetch, ...props }, ref) => (
-    <NextLink
-      href={href}
-      as={as}
-      prefetch={prefetch}
-      passHref
+export function Link(props: LinkProps<'a'>) {
+  return (
+    <MuiLink
+      component={NextLink}
       style={{ textDecoration: 'none', color: 'inherit' }}
-    >
-      <LinkMUI
-        ref={ref}
-        sx={{ textDecoration: 'none', color: 'inherit' }}
-        {...props}
-      />
-    </NextLink>
+      {...props}
+    />
   )
-)
-
-Link.displayName = 'Link'
-
-export { Link }
+}
